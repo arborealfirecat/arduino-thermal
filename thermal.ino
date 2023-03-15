c7bc0611763f046ae70b7ef6d111d9db2a65ea34
ArborealFireCat <arborealfirecat@gmail.com>
2023-03-15 13:11:27 +0000

    feat: adds basic thermal imaging

---
+++ thermal.ino
@@ -0,0 +1,144 @@
+#include "T4_V13.h"
+#include <TFT_eSPI.h>
+#include <SPI.h>
+#include "WiFi.h"
+#include <Wire.h>
+#include <Ticker.h>
+#include <Button2.h>
+#include <SD.h>
+#include <Adafruit_MLX90640.h>
+
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+Adafruit_MLX90640 mlx;
+
+SPIClass sdSPI(VSPI);
+#define IP5306_ADDR         0X75
+#define IP5306_REG_SYS_CTL0 0x00
+
+#define MINTEMP 20
+#define MAXTEMP 35
+
+uint8_t state = 0;
+char buff[512];
+float frame[32*24];
+uint16_t displayPixelWidth, displayPixelHeight;
+const uint16_t camColors[] = {0x480F,
+0x400F,0x400F,0x400F,0x4010,0x3810,0x3810,0x3810,0x3810,0x3010,0x3010,
+0x3010,0x2810,0x2810,0x2810,0x2810,0x2010,0x2010,0x2010,0x1810,0x1810,
+0x1811,0x1811,0x1011,0x1011,0x1011,0x0811,0x0811,0x0811,0x0011,0x0011,
+0x0011,0x0011,0x0011,0x0031,0x0031,0x0051,0x0072,0x0072,0x0092,0x00B2,
+0x00B2,0x00D2,0x00F2,0x00F2,0x0112,0x0132,0x0152,0x0152,0x0172,0x0192,
+0x0192,0x01B2,0x01D2,0x01F3,0x01F3,0x0213,0x0233,0x0253,0x0253,0x0273,
+0x0293,0x02B3,0x02D3,0x02D3,0x02F3,0x0313,0x0333,0x0333,0x0353,0x0373,
+0x0394,0x03B4,0x03D4,0x03D4,0x03F4,0x0414,0x0434,0x0454,0x0474,0x0474,
+0x0494,0x04B4,0x04D4,0x04F4,0x0514,0x0534,0x0534,0x0554,0x0554,0x0574,
+0x0574,0x0573,0x0573,0x0573,0x0572,0x0572,0x0572,0x0571,0x0591,0x0591,
+0x0590,0x0590,0x058F,0x058F,0x058F,0x058E,0x05AE,0x05AE,0x05AD,0x05AD,
+0x05AD,0x05AC,0x05AC,0x05AB,0x05CB,0x05CB,0x05CA,0x05CA,0x05CA,0x05C9,
+0x05C9,0x05C8,0x05E8,0x05E8,0x05E7,0x05E7,0x05E6,0x05E6,0x05E6,0x05E5,
+0x05E5,0x0604,0x0604,0x0604,0x0603,0x0603,0x0602,0x0602,0x0601,0x0621,
+0x0621,0x0620,0x0620,0x0620,0x0620,0x0E20,0x0E20,0x0E40,0x1640,0x1640,
+0x1E40,0x1E40,0x2640,0x2640,0x2E40,0x2E60,0x3660,0x3660,0x3E60,0x3E60,
+0x3E60,0x4660,0x4660,0x4E60,0x4E80,0x5680,0x5680,0x5E80,0x5E80,0x6680,
+0x6680,0x6E80,0x6EA0,0x76A0,0x76A0,0x7EA0,0x7EA0,0x86A0,0x86A0,0x8EA0,
+0x8EC0,0x96C0,0x96C0,0x9EC0,0x9EC0,0xA6C0,0xAEC0,0xAEC0,0xB6E0,0xB6E0,
+0xBEE0,0xBEE0,0xC6E0,0xC6E0,0xCEE0,0xCEE0,0xD6E0,0xD700,0xDF00,0xDEE0,
+0xDEC0,0xDEA0,0xDE80,0xDE80,0xE660,0xE640,0xE620,0xE600,0xE5E0,0xE5C0,
+0xE5A0,0xE580,0xE560,0xE540,0xE520,0xE500,0xE4E0,0xE4C0,0xE4A0,0xE480,
+0xE460,0xEC40,0xEC20,0xEC00,0xEBE0,0xEBC0,0xEBA0,0xEB80,0xEB60,0xEB40,
+0xEB20,0xEB00,0xEAE0,0xEAC0,0xEAA0,0xEA80,0xEA60,0xEA40,0xF220,0xF200,
+0xF1E0,0xF1C0,0xF1A0,0xF180,0xF160,0xF140,0xF100,0xF0E0,0xF0C0,0xF0A0,
+0xF080,0xF060,0xF040,0xF020,0xF800,};
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+
+    //Pin out Dump
+    Serial.printf("Current select %s version\n", BOARD_VRESION);
+    Serial.printf("TFT_MISO:%d\n", TFT_MISO);
+    Serial.printf("TFT_MOSI:%d\n", TFT_MOSI);
+    Serial.printf("TFT_SCLK:%d\n", TFT_SCLK);
+    Serial.printf("TFT_CS:%d\n", TFT_CS);
+    Serial.printf("TFT_DC:%d\n", TFT_DC);
+    Serial.printf("TFT_RST:%d\n", TFT_RST);
+    Serial.printf("TFT_BL:%d\n", TFT_BL);
+    Serial.printf("SD_MISO:%d\n", SD_MISO);
+    Serial.printf("SD_MOSI:%d\n", SD_MOSI);
+    Serial.printf("SD_SCLK:%d\n", SD_SCLK);
+    Serial.printf("SD_CS:%d\n", SD_CS);
+    Serial.printf("I2C_SDA:%d\n", I2C_SDA);
+    Serial.printf("I2C_SCL:%d\n", I2C_SCL);
+    Serial.printf("SPEAKER_PWD:%d\n", SPEAKER_PWD);
+    Serial.printf("SPEAKER_OUT:%d\n", SPEAKER_OUT);
+    Serial.printf("ADC_IN:%d\n", ADC_IN);
+    Serial.printf("BUTTON_1:%d\n", BUTTON_1);
+    Serial.printf("BUTTON_2:%d\n", BUTTON_2);
+    Serial.printf("BUTTON_3:%d\n", BUTTON_3);
+#ifdef BUTTON_4
+    Serial.printf("BUTTON_4:%d\n", BUTTON_4);
+#endif
+
+    tft.init();
+    tft.setRotation(0);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(1);
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextFont(1);
+    tft.setCursor(0, 0);
+
+    if (TFT_BL > 0) {
+        pinMode(TFT_BL, OUTPUT);
+        digitalWrite(TFT_BL, HIGH);
+    }
+
+    if (I2C_SDA > 0) {
+        Wire.begin(I2C_SDA, I2C_SCL);
+    }
+
+    Serial.println("Adafruit MLX9060 Test");
+    if (!mlx.begin(MLX90640_I2CADDR_DEFAULT, &Wire)) {
+      Serial.println("MLX90640 not found");
+      while (1) delay(10);
+    }
+    Serial.println("Found Adafruit MLX90640");
+
+    // Sensor Mode
+    // mlx.setMode(MLX90640_INTERLEAVED);
+    mlx.setMode(MLX90640_CHESS); // chess view (grid with boxes)
+
+    // Set resolution 16, 17, 18 or 19
+    mlx.setResolution(MLX90640_ADC_18BIT);
+
+    // Set refresh rate
+    mlx.setRefreshRate(MLX90640_2_HZ);
+
+    displayPixelWidth = tft.width() / 32;
+    displayPixelHeight = tft.height() / 32;
+    Serial.print("Height: "); Serial.println(tft.height());
+    Serial.print("Width: "); Serial.println(tft.width());
+//    Wire.setClock(1000000); // max 1 MHz
+}
+
+
+void loop() {
+    delay(500); // let the camera warm up
+    // get IR frame
+    if (mlx.getFrame(frame) != 0) {
+      Serial.println("failed to load frame from lens");
+      return;
+    }
+
+    Serial.println();
+
+    for (uint8_t h = 0; h < 24; h++) {
+      for (uint8_t w = 0; w < 32; w++) {
+        float t = frame[h*32 + w];
+
+        t = min<float>(t, MAXTEMP);
+        t = max<float>(t, MINTEMP);
+        uint8_t colorIndex = map(t, MINTEMP, MAXTEMP, 0, 255);
+        tft.fillRect(displayPixelWidth * w, displayPixelHeight * h, displayPixelHeight, displayPixelWidth, camColors[colorIndex]);
+      }
+    }
+}
